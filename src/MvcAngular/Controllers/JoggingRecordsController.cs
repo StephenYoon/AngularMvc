ac73404a32bb67f129ec09231ac5dc404605b749
Stephen <yoonic@gmail.com>
2019-01-19 17:31:49 -0500

    Setup of database & setup of api endpoints

---
+++ src/MvcAngular/Controllers/JoggingRecordsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using MvcAngular.Data;
+using MvcAngular.Models;
+
+namespace MvcAngular.Controllers
+{
+    public class JoggingRecordsController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/JoggingRecords
+        public IQueryable<JoggingRecord> GetJoggingRecords() => db.JoggingRecords;
+
+        // GET: api/JoggingRecords/5
+        [ResponseType(typeof(JoggingRecord))]
+        public async Task<IHttpActionResult> GetJoggingRecord(int id)
+        {
+            JoggingRecord joggingRecord = await db.JoggingRecords.FindAsync(id);
+            if (joggingRecord == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(joggingRecord);
+        }
+    }
+}
---
+++ src/MvcAngular/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using MvcAngular.Models;
+
+namespace MvcAngular.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("OktaConnectionString")
+        {
+        }
+
+        public static ApplicationDbContext Create() => new ApplicationDbContext();
+
+        public DbSet<JoggingRecord> JoggingRecords { get; set; }
+    }
+}
