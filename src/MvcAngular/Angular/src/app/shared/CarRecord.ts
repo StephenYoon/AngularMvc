c3fb493164126d7c1b181664205bb74a24fcb9c2
Stephen <yoonic@gmail.com>
2019-01-20 18:15:26 -0500

    Updated angular component to display data and fixed CarRecord.tx

--- src/MvcAngular/Angular/src/app/shared/CarRecord.ts
+++ src/MvcAngular/Angular/src/app/shared/CarRecord.ts
@@ -1,9 +1,9 @@
 export default class CarRecord {
   id: number;
   year: number;
-  Make: string;
-  Model: string;
-  Trim: string;
-  HorsePower: number;
-  WeightKilograms: number;
+  make: string;
+  model: string;
+  trim: string;
+  horsePower: number;
+  weightKilograms: number;
 }
--- src/MvcAngular/Controllers/CarsController.cs
+++ src/MvcAngular/Controllers/CarsController.cs
@@ -17,16 +17,19 @@ namespace MvcAngular.Controllers
         {
             _records = new List<Car>
             {
-                new Car { Id = 1, Year = 2018, Make = "Chevy", Model = "Camaro", Trim = "Exorcist", HorsePower = 1000, WeightKilograms = 2100 },
-                new Car { Id = 2, Year = 2018, Make = "Dodge", Model = "Challenger", Trim = "Demon", HorsePower = 840, WeightKilograms = 2300 },
-                new Car { Id = 3, Year = 2018, Make = "Ford", Model = "Mustang", Trim = "Boss", HorsePower = 750, WeightKilograms = 1900 }
+                new Car { Id = 1, Year = 2018, Make = "Chevy", Model = "Camaro", Trim = "ZL1 Exorcist", HorsePower = 1000, WeightKilograms = 1732 },
+                new Car { Id = 2, Year = 2018, Make = "Dodge", Model = "Challenger", Trim = "Demon", HorsePower = 840, WeightKilograms = 1939},
+                new Car { Id = 3, Year = 2018, Make = "Ford", Model = "Mustang", Trim = "Boss", HorsePower = 750, WeightKilograms = 1720 },
+                new Car { Id = 3, Year = 2019, Make = "Genesis", Model = "G70", Trim = "Dynamic", HorsePower = 365, WeightKilograms = 1670 }
             };
         }
 
-        // GET: api/cars
-        public List<Car> GetCars() => _records;
+        // GET: api/carrecords
+        [Route("api/carrecords")]
+        public List<Car> GetCarRecords() => _records;
 
         // GET: api/cars/5
+        [Route("api/carrecords/{id}")]
         [ResponseType(typeof(Car))]
         public Car GetCarRecord(int id)
         {
