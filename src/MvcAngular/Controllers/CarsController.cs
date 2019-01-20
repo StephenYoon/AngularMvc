ad7f2b7b7d01c713c1cb879190bdf703452d19be
Stephen <yoonic@gmail.com>
2019-01-19 21:04:28 -0500

    Changed topic from jogging to CARS! :)

---
+++ src/MvcAngular/Controllers/CarsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using MvcAngular.Models;
+
+namespace MvcAngular.Controllers
+{
+    public class CarsController : ApiController
+    {
+        private List<Car> _records;
+
+        public CarsController()
+        {
+            _records = new List<Car>
+            {
+                new Car { Id = 1, Year = 2018, Make = "Chevy", Model = "Camaro", Trim = "Exorcist", HorsePower = 1000, WeightKilograms = 2100 },
+                new Car { Id = 2, Year = 2018, Make = "Dodge", Model = "Challenger", Trim = "Demon", HorsePower = 840, WeightKilograms = 2300 },
+                new Car { Id = 3, Year = 2018, Make = "Ford", Model = "Mustang", Trim = "Boss", HorsePower = 750, WeightKilograms = 1900 }
+            };
+        }
+
+        // GET: api/cars
+        public List<Car> GetCars() => _records;
+
+        // GET: api/cars/5
+        [ResponseType(typeof(Car))]
+        public Car GetCarRecord(int id)
+        {
+            Car carRecord = _records.Where(r => r.Id == id).FirstOrDefault();
+            return carRecord != null ? carRecord : new Car();
+        }
+    }
+}
---
+++ src/MvcAngular/Models/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcAngular.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+
+        public int Year { get; set; }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public string Trim { get; set; }
+
+        public int HorsePower { get; set; }
+
+        public int WeightKilograms { get; set; }
+    }
+}
